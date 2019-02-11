28264f783de6a56f4faf251d8c9606f3f2d93cdd
Jesse Zelaya <jesse@seedcms.com>
2019-02-10 16:48:32 -0800

    PostCss autroprefix and production minification

--- slate.config.js
+++ slate.config.js
@@ -4,6 +4,8 @@
 // For more information, visit https://github.com/Shopify/slate/wiki/Slate-Configuration
 
 const path = require('path');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 module.exports = {
   'cssVarLoader.liquidPath': ['src/snippets/css-variables.liquid'],
@@ -14,5 +16,15 @@ module.exports = {
         'lodash-es': path.resolve('./node_modules/lodash-es'),
       },
     },
+  },
+  'webpack.postcss.plugins': (config) => {
+    const plugins = [autoprefixer];
+
+    // We only want to minify our CSS if we're building for production
+    if (process.env.NODE_ENV === 'production') {
+      plugins.push(cssnano(config.get('webpack.cssnano.settings')))
+    }
+
+    return plugins;
   },
 };
