3799125bb7fb53b94d5f5ecad25b7f7016d0a7b2
Jesse Zelaya <jesse@seedcms.com>
2019-05-18 13:21:39 -0700

    Created mobile nav drawer
    Init drawer and submenu toggling enabled

---
+++ src/scripts/components/mobile-nav.js
@@ -0,0 +1,39 @@
+import Drawer from '@/scripts/components/drawers'
+
+/**
+ * Mobile Nav Script
+ * ------------------------------------------------------------------------------
+ *
+ * @namespace mobileNav
+ */
+
+// Selectors
+const selectors = {
+  mobileNavId: 'MobileNav',
+  mobileNavToggle: '[data-mobile-nav-toggle]'
+};
+
+const cssClasses = {
+  activeBtn: 'is-active',
+  openMenu: 'is-open'
+};
+
+// Init mobile nav drawer
+const mobileNavDrawer = new Drawer('MobileNavDrawer', {position: 'left'});
+
+// Cached Selectors
+const mobileNav = document.getElementById(selectors.mobileNavId);
+const mobileNavToggle = mobileNav.querySelectorAll(selectors.mobileNavToggle);
+
+// Bind event handlers
+mobileNavToggle.forEach((button) => {
+  button.addEventListener('click', toggleSubMenu);
+});
+
+function toggleSubMenu(evt) {
+  const button = evt.currentTarget
+  const subMenu = button.nextElementSibling;
+
+  button.classList.toggle(cssClasses.activeBtn);
+  subMenu.classList.toggle(cssClasses.openMenu);
+}
--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -5,6 +5,8 @@ import 'lazysizes/plugins/bgset/ls.bgset';
 import 'lazysizes';
 import 'lazysizes/plugins/respimg/ls.respimg';
 
+import '@/scripts/components/mobile-nav'
+
 import '../../styles/theme.scss';
 import '../../styles/theme.scss.liquid';
 
