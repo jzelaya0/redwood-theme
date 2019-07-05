a3acd5d4bc97e02b7a5450de4b77f9d6d89431e5
Jesse Zelaya <jesse@seedcms.com>
2019-07-05 14:54:56 -0700

    Update theme-scripts cart package

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -11,11 +11,24 @@ import '../../styles/theme.scss';
 import '../../styles/theme.scss.liquid';
 
 import {focusHash, bindInPageLinks} from '@shopify/theme-a11y';
-import {cookiesEnabled} from '@shopify/theme-cart';
 
 // Common a11y fixes
 focusHash();
 bindInPageLinks();
+
+/**
+ * Browser cookies are required to use the cart. This function checks if
+ * cookies are enabled in the browser.
+ */
+function cookiesEnabled() {
+  let cookieEnabled = navigator.cookieEnabled;
+
+  if (!cookieEnabled) {
+    document.cookie = 'testcookie';
+    cookieEnabled = (document.cookie.indexOf('testcookie') !== -1);
+  }
+  return cookieEnabled;
+}
 
 // Apply a specific class to the html element for browser support of cookies.
 if (cookiesEnabled()) {
